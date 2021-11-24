99f7d0f97d66604f124d9d7aea3cecfcc787ad13
André Flores <andrejesusferflores@gmail.com>
2021-11-24 10:18:18 +0000

    Started on logic

--- mancala.js
+++ mancala.js
@@ -41,12 +41,95 @@ class Game{
 }
 
 class Board{
-    constructor(housesNumber, seedsNumber){
-        this.housesNumber = housesNumber;
-        this.seedsNumber = seedsNumber;
+    constructor(houseNumber, seedNumber){
+        this.houseNumber = houseNumber;
+        this.seedNumber = seedNumber;
+        this.playerRow = new Row(this.houseNumber, this.seedNumber);
+        this.adversaryRow = new Row(this.houseNumber, this.seedNumber);
+        this.playerRow.setNextRow(this.adversaryRow);
+        this.adversaryRow.setNextRow(this.playerRow);
     }
 
     createBoard() {
 
+    }
+}
+
+class Row {
+    constructor(houseNumber, seedNumber) {
+        this.houseNumber = houseNumber;
+        this.seedNumber = seedNumber;
+        this.warehouse = new Warehouse(null, this);
+        this.houses = []
+        for (let i = 0; i < this.houseNumber; i++) {
+            this.houses.push(new House(seedNumber, null, this));
+        }
+        for (let i = 0; i < this.houseNumber - 1; i++) {
+            this.houses[i].setNextHouse(this.houses[i + 1]);
+        }
+        this.houses[this.houseNumber - 1].nextHouse(this.warehouse);
+    }
+
+    setWareHouse(warehouse) {
+        this.warehouse = warehouse;
+        this.houses[this.houseNumber - 1].setNextHouse(warehouse);
+    }
+
+    setNextRow(row) {
+        this.warehouse.setNextHouse(row.houses[0]);
+    }
+
+    startSeed(houseNumber) {
+        this.houses[houseNumber].startSeed();
+    }
+}
+
+class House {
+    constructor(seedNumber, nextHouse, row) {
+        this.seedNumber = seedNumber;
+        this.nextHouse = nextHouse;
+        this.row = row;
+    }
+
+    setSeedNumber(seedNumber) {
+        this.seedNumber = seedNumber;
+    }
+
+    setNextHouse(nextHouse) {
+        this.nextHouse = nextHouse;
+    }
+
+    setRow(row) {
+        this.row = row;
+    }
+
+    startSeed() {
+        let tempSeeds = this.seedNumber;
+        this.seedNumber = 0;
+        this.nextHouse.seed(tempSeeds, this.row);
+    }
+
+    seed(seedNumber, row) {
+        if (seedNumber == 1) {
+            this.seedNumber++;
+        }
+        else {
+            this.seedNumber++;
+            this.nextHouse.seed(--seedNumber, row);
+        }
+    }
+}
+
+class Warehouse extends House {
+    constructor(nextHouse, row) {
+        super(0, nextHouse, row);
+    }
+
+    startSeed() {
+    }
+
+    seed(seedNumber, row) {
+        if (row == super.row) super.seed(seedNumber);
+        else super.nextHouse.seed(seedNumber);
     }
 }
