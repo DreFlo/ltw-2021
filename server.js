10ef49e547316cae56b6336dfe4b1ff1c4fbcea7
Skoubou9 <72798819+Skoubou9@users.noreply.github.com>
2022-01-12 08:57:48 +0000

    sad

--- server.js
+++ server.js
@@ -4,8 +4,16 @@ Acho que criei isto pra nada, mas ainda não sei
 
 var http = require('http');
 var fs = require('fs');
-var PORT = 8008;
+var PORT = 9066; //8966
 
+const server = http.createServer(function (request, response) {
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.end('Olá mundo\n');
+});
+
+server.listen(PORT);
+
+/*
 fs.readFile('./index.html', function(err, html){
     if(!err){
         http.createServer(function(request, response) {
@@ -14,4 +22,4 @@ fs.readFile('./index.html', function(err, html){
             response.end();
         }).listen(PORT);
     }
-});
+});*/
